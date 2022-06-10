ad0438b27d36c38df61ead30df10bdefd69a49da
Konstantin <tonkushin.konstantin@gmail.com>
2022-06-10 12:35:49 +0500

    practic 3.5

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from . import views
 
+boosts = views.BoostViewSet.as_view({ 
+    'get': 'list',
+    'post': 'create',
+})
+
+
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('call_click/', views.call_click),
+    path('', views.index, name='index'),
+    path('boosts/', boosts, name='boosts')
 ]
